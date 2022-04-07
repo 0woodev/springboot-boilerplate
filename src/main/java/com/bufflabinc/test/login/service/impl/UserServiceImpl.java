076b9fb221b58420558fabb8fb34124f6cfec38a
0woodev <0woodev@gmail.com>
2022-04-07 17:26:54 +0900

    ################ # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성 # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지 # 예) feat : 로그인 기능 추가 remove: 유저 직접생성 API 제거 # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # temp : 현재 진행중인 작업을 올리는 경우
    # git : git 관련 작업인 경우
    # design : CSS 등 사용자 UI 디자인 변경
    # comment : 필요한 주석 추가 및 변경
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # rename: 파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우
    # remove: 파일을 삭제하는 작업만 수행하는 경우
    ################

--- src/main/java/com/bufflabinc/test/login/service/impl/UserServiceImpl.java
+++ src/main/java/com/bufflabinc/test/login/service/impl/UserServiceImpl.java
@@ -1,16 +1,19 @@
 package com.bufflabinc.test.login.service.impl;
 
-import com.bufflabinc.test.login.controller.dto.UserRequestDto;
 import com.bufflabinc.test.login.controller.dto.UserResponseDto;
 import com.bufflabinc.test.login.domain.user.UserRepository;
 import com.bufflabinc.test.login.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
+@Service
 public class UserServiceImpl implements UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public List<UserResponseDto> getAllUsers() {
@@ -18,12 +21,4 @@ public class UserServiceImpl implements UserService {
                 .map(UserResponseDto::new)
                 .collect(Collectors.toList());
     }
-
-    @Override
-    public Long addUser(UserRequestDto user) {
-//        return userRepository.save(user);
-        return 1L;
-    }
-
-
 }
