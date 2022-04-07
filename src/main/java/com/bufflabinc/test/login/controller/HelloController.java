720f59892a5448d68926d924aaf369c3066be741
0woodev <0woodev@gmail.com>
2022-04-07 17:21:47 +0900

    ################ # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성 # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지 # 예) feat : 로그인 기능 추가 git: git commit message template 변경 # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함) temp: 추후에 oauth 와 jwt 를 같이 진행해볼 예정 ################ # 본문(구체적인 내용)을 아랫줄에 작성 # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내) feat: swagger 추가 ################ # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등) # 예) Close #7
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # temp : 현재 진행중인 작업을 올리는 경우
    # git : git 관련 작업인 경우
    # design : CSS 등 사용자 UI 디자인 변경
    # comment : 필요한 주석 추가 및 변경
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # rename: 파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우
    # remove: 파일을 삭제하는 작업만 수행하는 경우
    ################

--- src/main/java/com/bufflabinc/test/login/controller/HelloController.java
+++ src/main/java/com/bufflabinc/test/login/controller/HelloController.java
@@ -1,11 +1,25 @@
 package com.bufflabinc.test.login.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Api(value = "Hello", tags = "hello")
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
+    @Operation(summary = "핼로우 출력", description = "앙")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK!"),
+            @ApiResponse(code = 400, message = "BAD REQUEST!"),
+            @ApiResponse(code = 500, message = "INTERNAL SERVER ERROR!"),
+    })
     @GetMapping("/hello")
     public String hello() {
         return "Hello world!";
--- src/main/java/com/bufflabinc/test/login/controller/UserController.java
+++ src/main/java/com/bufflabinc/test/login/controller/UserController.java
@@ -1,29 +1,36 @@
 package com.bufflabinc.test.login.controller;
 
-import com.bufflabinc.test.login.controller.dto.UserRequestDto;
 import com.bufflabinc.test.login.controller.dto.UserResponseDto;
-import com.bufflabinc.test.login.domain.user.User;
 import com.bufflabinc.test.login.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Api(value = "User", tags = "User")
+@RequiredArgsConstructor
 @RequestMapping("/api/user")
+@RestController
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    private final UserService userService;
 
+
+    @Operation(summary = "전체 유저 조회", description = "전체 유저를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK!"),
+            @ApiResponse(code = 400, message = "BAD REQUEST!"),
+            @ApiResponse(code = 500, message = "INTERNAL SERVER ERROR!"),
+    })
     @GetMapping("/all")
     public ResponseEntity<List<UserResponseDto>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
-    }
-
-    @PostMapping("")
-    public ResponseEntity<Long> addUser(@RequestParam UserRequestDto requestDto) {
-        return ResponseEntity.ok(userService.addUser(requestDto));
     }
 }
